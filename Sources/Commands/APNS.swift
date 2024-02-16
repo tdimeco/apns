604a96498e6eaef9f75a9502eb855d10c5d8b810
Thomas Di Meco <git@tdimeco.fr>
2024-02-16 20:55:29 +0100

    Add version and about options

--- Sources/Commands/APNS.swift
+++ Sources/Commands/APNS.swift
@@ -1,6 +1,9 @@
 import Foundation
 import ArgumentParser
 
+// App version constant
+private let APP_VERSION = "1.0.0"
+
 @main
 struct APNS: AsyncParsableCommand {
 
@@ -13,4 +16,35 @@ struct APNS: AsyncParsableCommand {
             Send.self
         ]
     )
+
+    // MARK: Options
+
+    @Flag(
+        name: [.customShort("v"), .customLong("version")],
+        help: "Show the version number."
+    )
+    var showVersion = false
+
+    @Flag(
+        name: [.customLong("about")],
+        help: "Show more information about the command."
+    )
+    var showAbout = false
+
+    // MARK: Execution
+
+    func run() throws {
+        if showAbout {
+            throw CleanExit.message("""
+                APNS version \(APP_VERSION)
+
+                For more information about the APNS command, go to:
+                https://github.com/tdimeco/apns
+                """)
+        } else if showVersion {
+            throw CleanExit.message(APP_VERSION)
+        } else {
+            throw CleanExit.helpRequest()
+        }
+    }
 }
